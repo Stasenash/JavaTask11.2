7d8b5fecf9a1531300dfa8fbb987c398fa6b49ea
Stasenash <staslisenash@gmail.com>
2020-05-06 21:08:56 +0500

    Initial commit

---
+++ src/com/example/task02/PoolExample.java
@@ -0,0 +1,49 @@
+package com.example.task02;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PoolExample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // создаем пул для выполнения наших задач
+        //   максимальное количество созданных задач - 3
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                // не изменяйте эти параметры
+                3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(3));
+
+        // сколько задач выполнилось
+        AtomicInteger count = new AtomicInteger(0);
+
+        // сколько задач выполняется
+        AtomicInteger inProgress = new AtomicInteger(0);
+
+        // отправляем задачи на выполнение
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < 3; i++) {
+                final int number = (i + 1) * (j + 1);
+                Thread.sleep(10);
+
+                System.out.println("creating #" + number);
+                if (inProgress.intValue() < 3) executor.submit(() -> {
+                    int working = inProgress.incrementAndGet();
+                    System.out.println("start #" + number + ", in progress: " + working);
+                    try {
+                        // тут какая-то полезная работа
+                        Thread.sleep(Math.round(1000 + Math.random() * 2000));
+                    } catch (InterruptedException e) {
+                        // ignore
+                    }
+                    working = inProgress.decrementAndGet();
+                    System.out.println("end #" + number + ", in progress: " + working + ", done tasks: " + count.incrementAndGet());
+                    return null;
+                });
+            }
+            Thread.sleep(3000);
+        }
+        executor.shutdown();
+    }
+}
